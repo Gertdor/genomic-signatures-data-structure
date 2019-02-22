fa1218351a7e8c6bdd9c42c37a78483a55438986
Sebastian Norlin <wazazew@gmail.com>
2019-02-22 17:23:47 +0100

    added files to check distribution of distances in data

---
+++ src/python/numberDistributionExperiment.py
@@ -0,0 +1,8 @@
+from scipy import stats
+from random import randint
+from dataStructures.VPTreeElement import VPTreeElement
+import numpy as np
+
+numberList = [VPTreeElement(np.random.uniform(0,0.35,2)) for x in range(5000)]
+distances = [current.distance(other) for current in numberList for other in numberList]
+print(stats.describe(distances))
