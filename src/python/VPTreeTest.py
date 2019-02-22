11134e55dd54619c4e2716961e78ab7ab8498fbd
Sebastian Norlin <wazazew@gmail.com>
2019-02-22 17:20:59 +0100

    added test for low dim VPTree

--- src/python/VPTreeTest.py
+++ src/python/VPTreeTest.py
@@ -6,27 +6,36 @@ from dataStructures.VPTreeElement import VPTreeElement
 import cProfile
 
 k=1
-
-numberList = [VPTreeElement(np.array([randint(1,101),randint(1,101),randint(1,101)])) for x in range(5000)]
+dim = 10
+numberList = [VPTreeElement(np.random.uniform(0,3,dim )) for x in range(70000)]
 tree = VPTree.createVPTree(numberList)
-elementToCompare = VPTreeElement(np.array([50,2,17]))
 
-(NN, dist, ops) = VPTree.nearestNeighbour(tree,elementToCompare,k)
+
+numberOfElements = 100
+elements = [VPTreeElement(np.random.uniform(0,3,dim)) for x in range(numberOfElements)]
+
+NNS = [VPTree.nearestNeighbour(tree,elem,k) for elem in elements]
 #cProfile.run('VPTree.nearestNeighbour(tree,elementToCompare,k)')
 
-distances = [(elementToCompare.distance(x), x) for x in numberList]
-distances.sort(key=lambda x: x[0])
-NN.sort(key=lambda x:x[0])
+#distances = [(elementToCompare.distance(x), x) for x in numberList]
+#distances.sort(key=lambda x: x[0])
+#NN.sort(key=lambda x:x[0])
 
 
-print("number of ops", ops)
-for i in range(k):
-    dist = NN[i][0]
-    element = NN[i][1]
-    print("Best distances: ",round(distances[i][0],3))
-    print("Best element: ", distances[i][1].value)
-    print("Found Element: ", element.value.value)
-    print("Found distance: ", round(dist,3))
-    print("\n")
+#print("number of ops", ops)
+total_distance_calls = 0
+for NN in NNS:
+    #for i in range(k):
+       # dist = NN[i][0]
+       # element = NN[i][1]
+    dist = NN[0][0][0]
+    element = NN[0][0][1]
+    total_distance_calls+=NN[2]
+    #print("Best distances: ",round(distances[i][0],3))
+    #print("Best element: ", distances[i][1].value)
+    #print("Found Element: ", element.value.value)
+    #print("Found distance: ", round(dist,3))
+    #print("\n")
 
+print("avg number of distance calls", total_distance_calls/numberOfElements)
 #VPTree.toJson(tree) # Prints tree
--- src/python/dataStructures/VLMCElement.py
+++ src/python/dataStructures/VLMCElement.py
@@ -2,9 +2,10 @@ from clustering_genomic_signatures.distance import FrobeniusNorm
 
 class VPTreeVLMC:
 
-    def __init__(self, value):
+    def __init__(self, value, distance_function, identifier = None):
         self.value = value
-        self.dist_func = FrobeniusNorm()
+        self.dist_func = distance_function
+        self.identifier = identifier
     
     def distance(self, other):
         return(self.dist_func.distance(self.value, other.value))
