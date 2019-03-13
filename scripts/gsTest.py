c7064906060e437437531b2635fb21025afd350a
Sebastian Norlin <wazazew@gmail.com>
2019-03-13 11:26:24 +0100

    added ability to test the greedy factor

--- scripts/gsTest.py
+++ scripts/gsTest.py
@@ -20,25 +20,67 @@ def fullTree(elements, random_element, leaf_size):
     tree = VPTree.createVPTree(elements, random_element, max_leaf_size=leaf_size)
     return(tree)
 
-def multipleNNSearches(elements, args):
+def greedy_factor_test(elements, args):
+    greedy_factors = np.linspace(args.greedy_start, args.greedy_end, args.greedy_num_samples)
+    all_runs=[[] for i in range(args.greedy_num_samples)]
 
-    all_NNS = [partOfTree(elements,args,print_results=False) for i in range(args.number_of_runs)]
+    for run_nbr in range(args.number_of_runs):
+        
+        print("current run number:",run_nbr)
+        (tree_elems, search_elems) = _split_elements(elements,args)
+        tree = VPTree.createVPTree(tree_elems, random=args.random_vp, max_leaf_size=args.leaf_size)
+        for (i,greedy_factor) in enumerate(greedy_factors):
+            
+            run_NNS = [VPTree.nearestNeighbour(tree,elem, args.k, greedy_factor) for elem in search_elems]
+            run_stats = [(NN[1],NN[2]) for NN in run_NNS]
+            all_runs[i].extend(run_stats)
+        
+    for run in all_runs:
+        run_stats = stats.describe(run)
+        #print("minmax", run_stats.minmax)
+        
+        print("mean", run_stats.mean)
+        #print("variance", run_stats.variance)
+
+    with open("greedy_test.pickle","wb") as f:
+        pickle.dump((all_runs,greedy_factors),f)
+
+
+def multipleNNSearches(elements, args, print_results = True):
+    
+    all_NNS = [NNSearch(elements,args,print_results=False) for i in range(args.number_of_runs)]
     complete_stats = [(NN[1],NN[2]) for NNS in all_NNS for NN in NNS]
-    print(stats.describe(complete_stats))
+    if(print_results):
+        print(stats.describe(complete_stats))
+    return(complete_stats)
 
-def partOfTree(elems, args, print_results = True):
+def _split_elements(elems, args):
     
     if(args.randomize_elements):
         elements = elems.copy()
         np.random.shuffle(elements)
     else:
         elements = elems
-    numElemInTree = round(len(elements)*args.cutoff)
-    elementsChecked = len(elements) - numElemInTree
-    tree = VPTree.createVPTree(elements[0:numElemInTree],random=args.random_vp, max_leaf_size = args.leaf_size)
+    
+    num_elem_in_tree = round(len(elements)*args.cutoff)
+    if(args.number_of_searches == 0 or args.number_of_searches + num_elem_in_tree > len(elements)):
+        args.number_of_searches = len(elements)-num_elem_in_tree
+    
+    tree_elements = elements[0:num_elem_in_tree]
+    search_elements = elements[num_elem_in_tree:num_elem_in_tree+args.number_of_searches]
+
+    return(tree_elements, search_elements)
+
+def NNSearch(elements, args, print_results = True):
+    
+    (tree_elements, search_elements) = _split_elements(elements, args)
+    
+    elementsChecked = len(search_elements)
+
+    tree = VPTree.createVPTree(tree_elements,random=args.random_vp, max_leaf_size = args.leaf_size)
     
     start_time = time.time()
-    NNS = [VPTree.nearestNeighbour(tree, elem, args.k, args.greedy_factor) for elem in elements[numElemInTree:]]
+    NNS = [VPTree.nearestNeighbour(tree, elem, args.k, args.greedy_factor) for elem in search_elements]
     total_time = time.time()-start_time
 
 
@@ -86,12 +128,12 @@ def get_NNS_stats(NNS):
     return(totalNumberOfActions/elementsChecked, totalDist/elementsChecked)
 
 def generatePointTree(args):
-    numberList = [VPTreeElement(np.random.uniform(args.min_value, args.max_value, args.dim )) for x in range(args.number_of_searches)]
+    numberList = [VPTreeElement(np.random.uniform(args.min_value, args.max_value, args.dim )) for x in range(args.number_of_num_searches)]
     tree = VPTree.createVPTree(numberList, args.random_vp, args.leaf_size)
     return(tree)
 
 def number_NN(tree, args):
-    elements = [VPTreeElement(np.random.uniform(args.min_value,args.max_value, args.dim)) for x in range(args.number_of_searches)]
+    elements = [VPTreeElement(np.random.uniform(args.min_value,args.max_value, args.dim)) for x in range(args.number_of_num_searches)]
     NNS = [VPTree.nearestNeighbour(tree,elem,args.k,args.greedy_factor) for elem in elements]
     printNNS(NNS)
 
@@ -111,7 +153,7 @@ def calcOverlap(tree):
 
 parser = argparse.ArgumentParser(description="test args")
 parser.add_argument("--cutoff", type=float, default=0.8, help="How large portion of the data should be stored in the tree. The rest is searched for the nearest neighbour")
-parser.add_argument("--nn_test", action='store_false',help= "Should a nearest neighbour test be performed")
+parser.add_argument("--no_nn_test", action='store_true',help= "Should a nearest neighbour test be performed")
 parser.add_argument("--overlap", action='store_true',help= "should the overlap in the tree be calculated")
 parser.add_argument("--low_dim_tree", action='store_true', help="Should only the ATGC distance be used as distance metric")
 parser.add_argument("--num",action='store_true',help="Should whatever tests being run also be run with a tree based on random DIM dimensional points?")
@@ -123,11 +165,18 @@ parser.add_argument("--dim", type=int, default=5, help="The dimension of the ran
 parser.add_argument("--max_value", type=int, default=5, help="max value of the randomly generated numbers")
 parser.add_argument("--min_value", type=int, default=0, help="min value of the randomly generated numbers")
 parser.add_argument("--number_of_points", type=int, default=1000, help="Number of DIM dimensional points to generate")
-parser.add_argument("--number_of_searches", type=int, default=100, help="Number of DIM dimensional points to search for nearest neighbours with")
+parser.add_argument("--number_of_num_searches", type=int, default=100, help="Number of DIM dimensional points to search for nearest neighbours with")
 parser.add_argument("--greedy_factor",type=float, default=0, help="Determines how greedy the pruning is. A solution must be atleast this much better to be considered. Default is 0, that is, anything which has the ability to be better is considered.")
 parser.add_argument("--k",type=int,default=1, help="how many neighbours should be found? default=1")
 parser.add_argument("--randomize_elements",action='store_true',help="should the elements to be stored/quiered be randomized")
 parser.add_argument("--number_of_runs", type=int, default=1,help="How many times should an NN search be repeated for a specific setting.")
+parser.add_argument("--number_of_searches",type=int,default=0,help="how many of the elements should be used to NN search in the tree?")
+
+parser.add_argument("--greedy_test",action='store_true', help="This will run multiple runs with different greedy factors determined by --greedy_start, --greedy_end, --greedy_step")
+parser.add_argument("--greedy_start",type=float,default=0,help="start value of greedy_factor when running greedy__test")
+parser.add_argument("--greedy_end",type=float,default=4,help="end value of greedy_factor when running greedy_test")
+parser.add_argument("--greedy_num_samples",type=int,default=20,help="number of samples to take between greedy_start and greedy_end when running greedy_test")
+parser.add_argument("--greedy_test_file_name",default="greedy_test_data.pickle",help="where should the data be saved after the greedy_factor_test has been run")
 
 add_parse_vlmc_args(parser)
 add_distance_arguments(parser)
@@ -147,15 +196,19 @@ if(args.overlap):
         print("vlmc tree overlap")
         calcOverlap(vlmc_tree)
 
-if(args.nn_test):
+
+if(not args.no_nn_test):
     if(args.num):
         point_tree = generatePointTree(args)
         number_NN(point_tree,args)
     if(not args.no_gs):
         if(args.number_of_runs==1):
-            partOfTree(elements, args)
+            NNSearch(elements, args)
         else:
             multipleNNSearches(elements,args)
+
+if(args.greedy_test):
+    greedy_factor_test(elements,args)    
 
 if(args.low_dim_tree):
     lowDimTree(vlmcs, elements)
