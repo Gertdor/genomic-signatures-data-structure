ab4ae94638152726c29879ccacb5b25925ad4180
Sebastian Norlin <wazazew@gmail.com>
2019-04-04 16:08:03 +0200

    added vp forest

---
+++ gs-data-structures/dataStructures/VPForest.py
@@ -0,0 +1,43 @@
+from dataStructures.VPTree import VPTree
+from operator import itemgetter
+import numpy as np
+
+
+class VPForest:
+    def __init__(values, random=True, max_leaf_size=1):
+        self.forest = VPForest._create_VP_forest(values, random, max_leaf_size)
+
+    def _create_VP_forest(values, random, max_leaf_size):
+
+        paired_values = [(value.get_gc(), value) for value in values]
+        # sorted_values.sort(key=itemgetter(0))
+        cutoff_points = [0] + np.arange(0.25, 0.76, 0.02).tolist() + [1]
+        cutoff_ranges = zip(cutoff_points[0:-1], cutoff_points[1:])
+        span = 0.12
+
+        forest = {
+            (low, high): VPForest._generate_tree(
+                paired_values, low, high, span, random, max_leaf_size
+            )
+            for low, high in cutoff_ranges
+        }
+        return forest
+
+    def _generate_tree(values, low_gc, high_gc, span, random, max_leaf_size):
+
+        values_to_use = [
+            value[1]
+            for value in values
+            if value[0] > low_gc - span and value[0] < high_gc + span
+        ]
+        return VPTree.createVPTree(values_to_use, random, max_leaf_size)
+
+    def nearest_neighbor(self, point, k=1, greedy_factor=1, gc_pruning=False):
+        # TODO Can do some smarter implementation of the keys here, if it is slow
+
+        gc_content = point.get_gc()
+        for key in forest:
+            if gc_content > key[0] and gc_content < key[1]:
+                return VPTree.nearestNeighbour(
+                    forest[key], point, k, greedy_factor, gc_pruning
+                )
